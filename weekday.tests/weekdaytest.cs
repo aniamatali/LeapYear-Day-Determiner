b72960e5106b385ffc2119d344b747f6a5573350
alvin niamatali and hansen hansen <hansen@>
2017-10-19 12:38:23 -0700

    calculates number of days between 1/1/1 to date entered in test models all tests passing

---
+++ weekday.tests/weekdaytest.cs
@@ -0,0 +1,105 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System;
+using Weekday.Models;
+
+namespace Weekday.Tests
+{
+
+  [TestClass]
+  public class WeekdayerTests
+  {
+    [TestMethod]
+    public void GetValue1Year()
+    {
+      //Arrange
+      Weekdayer newDay = new Weekdayer(1,1,2);
+
+      int output = 366;
+
+      //Act
+      int result = (newDay.dayCalc());
+      Console.WriteLine("result " + result);
+
+      //Assert
+      Assert.AreEqual(result, output);
+
+    }
+
+    [TestMethod]
+    public void GetValue1Leap()
+    {
+      //Arrange
+      Weekdayer newDay = new Weekdayer(1,1,5);
+
+      int output = 1462;
+
+      //Act
+      int result = (newDay.dayCalc());
+      Console.WriteLine("result " + result);
+
+      //Assert
+      Assert.AreEqual(result, output);
+
+    }
+
+    [TestMethod]
+    public void GetValueLeapCentury()
+    {
+      //Arrange
+      Weekdayer newDay = new Weekdayer(1,1,100);
+
+      int output = 36160;
+
+      //Act
+      int result = (newDay.dayCalc());
+      Console.WriteLine("result " + result);
+
+      //Assert
+      Assert.AreEqual(result, output);
+
+    }
+
+    [TestMethod]
+    public void GetValueLeapMillenium()
+    {
+      //Arrange
+      Weekdayer newDay = new Weekdayer(1,1,1000);
+
+      int output = 364878;
+
+      //Act
+      int result = (newDay.dayCalc());
+      Console.WriteLine("result " + result);
+
+      //Assert
+      Assert.AreEqual(result, output);
+
+    }
+
+    [TestMethod]
+    public void GetValueLeapBeforeLeapYear()
+    {
+      //Arrange
+      Weekdayer newDay = new Weekdayer(1,1,12);
+
+      int output = 4017;
+
+      //Act
+      int result = (newDay.dayCalc());
+      Console.WriteLine("result " + result);
+
+      //Assert
+      Assert.AreEqual(result, output);
+
+    }
+
+
+
+
+      // public void Dispose()
+      // {
+      //   Weekdayer.ClearAll();
+      // }
+    }
+  }
---
+++ weekday/Models/weekday.cs
@@ -0,0 +1,69 @@
+using System.Collections.Generic;
+using System;
+
+namespace Weekday.Models
+{
+  public class Weekdayer
+  {
+    private int _inputDay;
+    private int _inputMonth;
+    private int _inputYear;
+    public int totalDay = 1;
+    Dictionary<int,int> month = new Dictionary<int,int>(){
+      {1,31},{2,28},{3,31},{4,30},{5,31},{6,30},{7,31},{8,31},{9,30},{10,31},{11,30},{12,31} };
+
+
+    public Weekdayer(int inputMonth, int inputDay, int inputYear)
+    {
+      _inputDay = inputDay;
+      _inputMonth = inputMonth;
+      _inputYear = inputYear;
+
+    }
+
+    public int GetDay()
+    {
+      return _inputDay;
+    }
+
+    public int GetMonth()
+    {
+      return _inputMonth;
+    }
+
+
+    public int GetYear()
+    {
+      return _inputYear;
+    }
+
+
+    public int dayCalc()
+    {
+      int dayMonth = 0;
+      int leapDay = 0;
+      int leapDay100 = 0;
+      int leapDay400 = 0;
+      int beforeLeap = 0;
+
+      leapDay = _inputYear / 4;
+      leapDay100 = _inputYear / 100;
+      leapDay400 = _inputYear / 400;
+
+      for(var i=1;i<_inputMonth;i++)
+      {
+        dayMonth += month[i];
+      }
+
+      if(dayMonth < 59 && _inputYear % 4 == 0 && _inputYear % 100 != 0)
+      {
+        beforeLeap = 1;
+      }
+
+      totalDay += ((_inputYear-1)*365)+ (_inputDay-1) + dayMonth + leapDay - leapDay100 + leapDay400 - beforeLeap;
+
+      return totalDay;
+    }
+
+    }
+  }
